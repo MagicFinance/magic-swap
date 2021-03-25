040246bcf0a75483befb50506bdc3988382cd31b
SemmyShow <magic.finance.defi@gmail.com>
2021-03-25 11:08:38 +0200

    a

---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x8688BeCe95EaFbd89a2Abc7183c348811838B46f',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0xa9ab2633a01f38a398e820ca1518545b385402c6'
+}
+
+export default priceContracts
